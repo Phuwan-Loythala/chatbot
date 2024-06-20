1c5b3632c758fab5ed4b4e0a277be14f7ad30b0c
Phuwan Loythala <101921334+Phuwan-Loythala@users.noreply.github.com>
2024-06-20 21:37:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-st.title('🎈 App Name')
+st.set_page_config(page_title="Chatbot Prototype for Mental Health Diagnosis")
 
 st.write('Hello world!')
